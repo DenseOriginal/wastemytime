a0bcbade0e29873b535a73f917b556f394cb675d
DenseOriginal <48748922+DenseOriginal@users.noreply.github.com>
2019-12-12 21:34:39 +0100

    I Dont Know

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IGame {
+  title: string,
+  desc: string,
+  link: string
+}
+
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.scss']
 })
 export class GamesComponent implements OnInit {
+  gamesCollection: IGame[] = [
+    {
+      title: 'Higher Or Lower',
+      desc: 'Just Higher Or Lower',
+      link: 'higherOrLower'
+    },
+    {
+      title: 'Memory Game',
+      desc: 'Classic Memory Game',
+      link: 'memory'
+    }
+  ]
 
   constructor() { }
 
